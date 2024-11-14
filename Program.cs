5497e37b1089f9580d29d4ab73061dbbbef71115
devCioo <dbienkowski@op.pl>
2024-11-14 18:49:40 +0100

    Update to .NET 9
    Updated project to .NET 9

--- Program.cs
+++ Program.cs
@@ -20,9 +20,9 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseStaticFiles();
 app.UseAntiforgery();
 
+app.MapStaticAssets();
 app.MapRazorComponents<App>()
 	.AddInteractiveServerRenderMode();
 
