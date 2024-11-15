467e6101ee359adc45afabbe856df60e153f7d32
devCioo <dbienkowski@op.pl>
2024-11-15 23:43:10 +0100

    Add chess board
    Added default chess board allowing to drag and drop chess pieces between fields and fixed font references

---
+++ Utils/BoardUtils.cs
@@ -0,0 +1,9 @@
+﻿namespace Cheesse.Utils
+{
+	public static class BoardUtils
+	{
+		public static char GetColumnName(int column) => (char)(column + 'a');
+		public static int GetColumnNumber(char column) => column - 'a';
+		public static int GetRowName(int row) => 8 - row;
+	}
+}
